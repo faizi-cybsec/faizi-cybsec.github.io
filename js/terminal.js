4dbbfa5dc855db6741e3ffebcbe8f6063006a340
faizi-cybsec <faizi.cybsec@gmail.com>
2026-07-01 09:29:07 +0500

    Create terminal.js

---
+++ js/terminal.js
@@ -0,0 +1,64 @@
+const commands = {
+    help: `
+Available commands:
+
+whoami
+skills
+projects
+education
+experience
+certs
+contact
+clear
+neofetch
+`,
+
+    whoami: `
+Faizan Ahmad
+Cybersecurity Student
+Security Research | Cyber Defense
+`,
+
+    skills: `
+Network Security
+Information Security
+Linux Fundamentals
+Cyber Defense
+Security Research
+`,
+
+    projects: `
+Network Reconnaissance Lab
+Web Security Testing Lab
+Home SOC Lab
+`,
+
+    education: `
+BS Cyber Security
+2nd Semester Student
+`,
+
+    experience: `
+SEO Specialist
+Lead Generation Specialist
+`,
+
+    certs: `
+Digital Marketing
+HP Business Email Writer
+Office Management
+`,
+
+    contact: `
+Email: work.fayzan@gmail.com
+Github: faizi-cybsec
+LinkedIn: fayzn-ahmed
+`,
+
+    neofetch: `
+OS: Kali Linux
+User: faizan
+Shell: bash
+Role: Cybersecurity Student
+`
+};
